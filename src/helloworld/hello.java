9154e0cfdf814a58e55391461238cf6c9d185a1e
jtresman <jtresman@users.noreply.github.com>
2014-09-17 12:16:51 -0600

    Update hello.java

--- src/helloworld/hello.java
+++ src/helloworld/hello.java
@@ -6,6 +6,7 @@ public class hello {
 		System.out.println("Hello World!"); //Display the string
 		System.out.println("A comment!");
 		System.out.println("Another Comment");
+		//A test comment
 	}
 	
 }
